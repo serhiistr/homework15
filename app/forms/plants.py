03feccca753dc80e14ac29d42609e689b99033f5
serhiistr <sergaudit2019@gmail.com>
2023-01-14 07:30:31 +0200

    first commit

---
+++ app/forms/plants.py
@@ -0,0 +1,9 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+
+
+class PlantsForm(FlaskForm):
+
+    title = StringField("Title")
+    location = StringField("Location")
+    submit = SubmitField("OK")
